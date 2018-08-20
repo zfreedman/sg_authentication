b71dd30f2b3712bc189721b1bcf51197b2f17a1e
zfreedman <zfreedman92@gmail.com>
2018-08-20 13:54:47 -0700

    feat: Added basic server setup to listen on specified port

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
 // main starting point of app (point of entry when server starts)
+
+// imports
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+// create app
+const app = express();
+
+// app setup
+
+
+// server setup
+const port = process.env.PORT || 9224;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server listening on port:", port);
