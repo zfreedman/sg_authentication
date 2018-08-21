0d9cda3932952a0819a7879212ac6ba6cb3eae22
zfreedman <zfreedman92@gmail.com>
2018-08-21 13:14:50 -0700

    feat: Wired up <Signup /> to app with react router

---
+++ client/src/components/auth/signup.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Signup extends React.Component {
+  render () {
+    return (
+      <form>
+        <fieldset>
+          <label>Email</label>
+        </fieldset>
+
+        <fieldset>
+          <label>Password</label>
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+export default Signup;
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/app";
+import Signup from "./components/auth/signup";
 import Welcome from "./components/welcome";
 
 ReactDOM.render(
   <BrowserRouter>
     <App>
       <Route path="/" component={Welcome} exact />
+      <Route path="/signup"component={Signup} />
     </App>
   </BrowserRouter>,
   document.querySelector("#root")
