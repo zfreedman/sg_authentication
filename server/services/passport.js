b262f1a597aca5a31e2d26e80cde09db10be60b4
zfreedman <zfreedman92@gmail.com>
2018-08-21 10:53:57 -0700

    feat: Added JWT strategy for user login

--- server/services/passport.js
+++ server/services/passport.js
@@ -4,3 +4,30 @@ const passport = require("passport");
 
 const config = require("../config");
 const User = require("../models/user");
+
+// setup options for jwt strategy
+const jwtOptions = {
+  jwFromRequest: ExtractJwt.fromHeader("authorization"),
+  secretOrKey: config.secret
+};
+
+// create jwt strategy (called whenever a user has to be authenticated
+// with a jwt token)
+const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
+
+  // see is the user id in the payload exists in database
+  // if so, call "done" with that user
+  // otherwise, call done without user object
+  User.findById(payload.sub, function (err, user) {
+    if (err)
+      return done(err, false);
+
+    if (user)
+      done(null, user);
+    else
+      done(null, false);
+  });
+});
+
+// tell passport to use this strategy
+passport.use(jwtLogin);
