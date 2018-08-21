cd7d9a59b714978fb08c82e1bc2343c5feda157a
zfreedman <zfreedman92@gmail.com>
2018-08-21 13:52:39 -0700

    feat: Wired up onSubmit function to grab form properties from redux-form

--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -1,19 +1,41 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
 class Signup extends React.Component {
   render () {
+
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit)}>
         <fieldset>
           <label>Email</label>
+          <Field
+            autoComplete="none"
+            component="input"
+            name="email"
+            type="text"
+          />
         </fieldset>
 
         <fieldset>
           <label>Password</label>
+          <Field
+            autoComplete="none"
+            component="input"
+            name="password"
+            type="password"
+          />
         </fieldset>
+
+        <button>Sign up</button>
       </form>
     );
   }
+
+  onSubmit = (formProps) => {
+    console.log(formProps);
+  };
 }
 
-export default Signup;
+export default reduxForm({ form: "signup" })(Signup);
