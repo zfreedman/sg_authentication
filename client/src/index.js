ff88fed581eea6afdf882a58d7b8cda8597cdb36
zfreedman <zfreedman92@gmail.com>
2018-08-21 13:24:13 -0700

    feat: Began fleshing out initial redux state for authentication
    Just built out start reducer for auth

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
 import { BrowserRouter, Route } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/app";
+import reducers from "./reducers";
 import Signup from "./components/auth/signup";
 import Welcome from "./components/welcome";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route path="/" component={Welcome} exact />
-      <Route path="/signup"component={Signup} />
-    </App>
-  </BrowserRouter>,
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" component={Welcome} exact />
+        <Route path="/signup"component={Signup} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector("#root")
 );
