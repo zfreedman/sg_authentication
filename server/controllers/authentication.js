440c0cdb43b938f87370e91bb5fa4bd1338beb1c
zfreedman <zfreedman92@gmail.com>
2018-08-21 10:30:37 -0700

    feat: Sending back JWT to user upon successful signup

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,16 @@
+const jwt = require("jwt-simple");
 const User = require("../models/user");
+
+const config = require("../config");
+
+function tokenForUser (user) {
+  const timestamp = new Date().getTime();
+
+  return jwt.encode({
+    iat: timestamp,
+    sub: user.id
+  }, config.secret);
+}
 
 exports.signup = function (req, res, next) {
   // see if user with given email exists
@@ -24,15 +36,13 @@ exports.signup = function (req, res, next) {
       const user = new User({
         email: email,
         password: password
-      })
-        .save(function (err) {
-          if (err)
-            return next(err);
+      });
+      user.save(function (err) {
+        if (err)
+          return next(err);
 
-          // respond to request indicating the user was created
-          res.json({ success: true });
-        });
-
-
+        // respond to request indicating the user was created
+        res.json({ token: tokenForUser(user) });
+      });
   });
 }
