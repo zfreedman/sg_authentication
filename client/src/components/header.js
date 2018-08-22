983d3c26dde3b4ea877a8f369001c35cca76d24b
zfreedman <zfreedman92@gmail.com>
2018-08-22 12:57:23 -0700

    feat: Added conditional header <Link> rendering

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,19 +1,33 @@
 import { Link } from "react-router-dom";
 import React from "react";
-// import { connect } from "react-redux";
+import { connect } from "react-redux";
 
 class Header extends React.Component {
   render () {
+    return this.renderLinks();
+  }
+
+  renderLinks () {
+    console.log(this.props.authenticated);
     return (
-      <div>
-        <Link to="/">Redux Auth</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signout">Sign Out</Link>
-        <Link to="/feature">Feature</Link>
-      </div>
+      this.props.authenticated
+        ? (
+            <div>
+              <Link to="/">Redux Auth</Link>
+              <Link to="/signout">Sign Out</Link>
+              <Link to="/feature">Feature</Link>
+            </div>
+          )
+        : (
+            <div>
+              <Link to="/">Redux Auth</Link>
+              <Link to="/signup">Sign Up</Link>
+              <Link to="/signin">Sign In</Link>
+            </div>
+          )
     );
   }
 }
 
-export default Header;
+let mapStateToProps = ({ auth }) => ({ authenticated: auth.authenticated });
+export default connect(mapStateToProps)(Header);
